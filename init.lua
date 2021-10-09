5d80ba161bcef55a41a14b994f357cccc885e671
William Mathewson <wncmathewson@me.com>
2021-10-09 17:28:22 +0100

    Remove leading `nocompatible` comment

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
--- set nocompatible              " be iMproved, required
-
+-- NOTE: Can this plugins require be deferred to load at the end?
+-- Does that need lazy loading of all plugins?
 require("plugins")
 
 require("utils")
