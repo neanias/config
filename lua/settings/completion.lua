c856a541d1b2407cc754b517c737945c676d2ae9
Will Mathewson <william.mathewson@freeagent.com>
2021-10-28 14:36:33 +0100

    Update autopairs cmp integration configuration
    This was changed as part of a breaking change in nvim-cmp, using
    differing hooks internally.

--- lua/settings/completion.lua
+++ lua/settings/completion.lua
@@ -32,12 +32,13 @@ local function reverse_tab_mapping(fallback)
 end
 
 cmp.setup({
-  sources = {
+  sources = cmp.config.sources({
     { name = "nvim_lsp" },
     { name = "luasnip" },
-    { name = "buffer" },
     { name = "path" },
-  },
+  }, {
+    { name = "buffer" },
+  }),
   snippet = {
     expand = function(args)
       -- For `luasnip` user.
@@ -47,22 +48,20 @@ cmp.setup({
   mapping = {
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    -- ['<C-Space>'] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.close(),
-    -- ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
     ["<Tab>"] = cmp.mapping(tab_mapping, { "i", "s" }),
     ["<S-Tab>"] = cmp.mapping(reverse_tab_mapping, { "i", "s" }),
   },
 })
 
 -- you need setup cmp first put this after cmp.setup()
-require("nvim-autopairs.completion.cmp").setup({
-  map_cr = true, --  map <CR> on insert mode
-  map_complete = true, -- it will auto insert `(` (map_char) after select function or method item
-  auto_select = true, -- automatically select the first item
-  insert = false, -- use insert confirm behavior instead of replace
-  map_char = { -- modifies the function or method delimiter by filetypes
-    all = "(",
-    tex = "{",
-  },
-})
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+cmp.event:on(
+  "confirm_done",
+  cmp_autopairs.on_confirm_done({
+    map_char = { -- modifies the function or method delimiter by filetypes
+      tex = "{",
+    },
+  })
+)
