39b237934a2a7919276a1f73d59b23f9585fd3aa
Will Mathewson <william.mathewson@freeagent.com>
2021-11-02 12:02:08 +0000

    Run stylua over files

--- lua/settings/git.lua
+++ lua/settings/git.lua
@@ -5,15 +5,20 @@ local neogit = require("neogit")
 neogit.setup({
   integrations = {
     diffview = true,
-  }
+  },
 })
 
 wk.register({
   g = {
     name = "+neogit",
     s = { neogit.open, "Open Neogit viewer" },
-    c = { function() neogit.open({ "commit" }) end, "Open Neogit commit dialog" },
-  }
+    c = {
+      function()
+        neogit.open({ "commit" })
+      end,
+      "Open Neogit commit dialog",
+    },
+  },
 }, {
   prefix = "<leader>",
   noremap = true,
