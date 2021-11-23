34b1e13c5509cf9a98cb495625a97ba367a41c5c
Will Mathewson <william.mathewson@freeagent.com>
2021-11-23 09:26:39 +0000

    Read all visible buffers for cmp-buffer

--- lua/settings/completion.lua
+++ lua/settings/completion.lua
@@ -39,6 +39,15 @@ cmp.setup({
   }, {
     { name = "buffer" },
   }),
+  buffer = {
+    get_bufnrs = function()
+      local bufs = {}
+      for _, win in ipairs(vim.api.nvim_list_wins()) do
+        bufs[vim.api.nvim_win_get_buf(win)] = true
+      end
+      return vim.tbl_keys(bufs)
+    end,
+  },
   snippet = {
     expand = function(args)
       -- For `luasnip` user.
