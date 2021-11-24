a574a129cfb95567966bbceace99eaa72c1667f9
Will Mathewson <william.mathewson@freeagent.com>
2021-11-24 20:01:30 +0000

    Remove treesitter-refactor navigation mappings
    Most of these are handled by Telescope, so we don't need them in the
    Treesitter configuration itself.

--- lua/settings/treesitter.lua
+++ lua/settings/treesitter.lua
@@ -27,16 +27,6 @@ ts_config.setup({
         smart_rename = "grr",
       },
     },
-    navigation = {
-      enable = true,
-      keymaps = {
-        goto_definition = "gnd",
-        list_definitions = "gnD",
-        list_definitions_toc = "gO",
-        goto_next_usage = "<a-*>",
-        goto_previous_usage = "<a-#>",
-      },
-    },
   },
   rainbow = {
     enable = true,
