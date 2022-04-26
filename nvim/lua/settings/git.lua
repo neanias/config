25866d94c92d9a7e3bb22d83d48159e90d649abe
Will Mathewson <william.mathewson@freeagent.com>
2022-04-26 13:10:23 +0100

    Add mappings for hopping to git hunks

--- nvim/lua/settings/git.lua
+++ nvim/lua/settings/git.lua
@@ -13,3 +13,32 @@ wk.register({
   prefix = "<leader>",
   noremap = true,
 })
+
+local gs = require("gitsigns")
+
+wk.register({
+  ["]c"] = {
+    function()
+      if vim.wo.diff then
+        return "]c"
+      end
+      vim.schedule(function()
+        gs.next_hunk()
+      end)
+      return "<Ignore>"
+    end,
+    "Go to next hunk",
+  },
+  ["[c"] = {
+    function()
+      if vim.wo.diff then
+        return "]c"
+      end
+      vim.schedule(function()
+        gs.prev_hunk()
+      end)
+      return "<Ignore>"
+    end,
+    "Go to previous hunk",
+  },
+})
