8b2db2f46fca90ab75189ff6d44e25d7f7e4b771
Will Mathewson <william.mathewson@freeagent.com>
2022-10-27 11:50:11 +0100

    Show macro recording in lualine

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -165,6 +165,15 @@ require("packer").startup({
         require("lualine").setup({
           options = { theme = "auto" },
           extensions = { "nvim-tree", "fugitive" },
+          sections = {
+            lualine_x = {
+              {
+                require("noice").api.statusline.mode.get,
+                cond = require("noice").api.statusline.mode.has,
+                color = { fg = "#ff9e64" },
+              },
+            },
+          },
         })
       end,
     })
