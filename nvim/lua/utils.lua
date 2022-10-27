95654891b710355f1f99379c80fcf88f6ab47c6b
Will Mathewson <william.mathewson@freeagent.com>
2022-10-27 11:47:14 +0100

    Remove unused utils method

--- nvim/lua/utils.lua
+++ nvim/lua/utils.lua
@@ -1,12 +1,3 @@
--- need a map method to handle the different kinds of key maps
-function map(mode, combo, mapping, opts)
-  local options = { noremap = true }
-  if opts then
-    options = vim.tbl_extend("force", options, opts)
-  end
-  vim.api.nvim_set_keymap(mode, combo, mapping, options)
-end
-
 local function atTipOfUndo()
   local tree = vim.fn.undotree()
   return tree.seq_last == tree.seq_cur
