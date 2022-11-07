dddf079db31b26a993f33868abdaa4bae41bfcf6
Will Mathewson <william.mathewson@freeagent.com>
2022-11-07 12:03:53 +0000

    Use new cmp-lsp capabilities strategy

--- nvim/lua/settings/lspconfig.lua
+++ nvim/lua/settings/lspconfig.lua
@@ -82,16 +82,13 @@ end
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
 local servers = { "solargraph" }
-for _, lsp in ipairs(servers) do
-  local capabilities = vim.lsp.protocol.make_client_capabilities()
-  capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup({
     capabilities = capabilities,
     on_attach = on_attach,
-    flags = {
-      debounce_text_changes = 150,
-    },
   })
 end
 
