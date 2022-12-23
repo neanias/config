21aa8a866b3741ffabc1f9c6ed4615c6990d0d7c
Will Mathewson <william.mathewson@freeagent.com>
2022-12-23 11:55:18 +0000

    Remove replaced settings files

--- nvim/lua/settings/ruby.lua
+++
@@ -1,5 +0,0 @@
--- Set the syntax for .rbapi files to be Ruby
-vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead", "BufReadPost" }, {
-  pattern = "*.rbapi",
-  command = [[set syntax=ruby]],
-})
--- nvim/lua/settings/splitjoin.lua
+++
@@ -1,8 +0,0 @@
-local wk = require("which-key")
-
-wk.register({
-  j = { "<Cmd>SplitjoinSplit<CR>", "Smart split 2 lines using SplitJoin" },
-  k = { "<Cmd>SplitjoinJoin<CR>", "Smart join 2 lines using SplitJoin" },
-}, {
-  prefix = "s",
-})
