c560b524c4079bb570d553ae36020c1d8d842235
Will Mathewson <william.mathewson@freeagent.com>
2023-01-09 17:05:54 +0000

    Load dressing.nvim lazily

--- nvim/lua/settings/plugins/dressing.lua
+++ nvim/lua/settings/plugins/dressing.lua
@@ -1,7 +1,19 @@
 local M = {
   "stevearc/dressing.nvim",
-  event = "VeryLazy",
 }
+
+function M.init()
+  ---@diagnostic disable-next-line: duplicate-set-field
+  vim.ui.select = function(...)
+    require("lazy").load({ plugins = { "dressing.nvim" } })
+    return vim.ui.select(...)
+  end
+  ---@diagnostic disable-next-line: duplicate-set-field
+  vim.ui.input = function(...)
+    require("lazy").load({ plugins = { "dressing.nvim" } })
+    return vim.ui.input(...)
+  end
+end
 
 function M.config()
   require("dressing").setup({
