1eef9e138e54910abfa51153a9d3d31db283517e
Will Mathewson <william.mathewson@freeagent.com>
2023-01-10 13:11:22 +0000

    Make Telescope load lazily

--- nvim/lua/settings/mappings.lua
+++ nvim/lua/settings/mappings.lua
@@ -4,8 +4,6 @@ local gs = require("gitsigns")
 local hop = require("hop")
 local hop_hint = require("hop.hint")
 local neotest = require("neotest")
-local telescope = require("telescope")
-local telescope_builtin = require("telescope.builtin")
 
 wk.setup({
   show_help = false,
@@ -74,20 +72,11 @@ wk.register({
     h = { "<cmd>Telescope help_tags<cr>", "Search for help tags" },
     j = { "<cmd>Telescope jumplist<cr>", "Search through jumplist" },
     l = { "<cmd>Telescope current_buffer_fuzzy_find<cr>", "Search through lines in the buffer" },
-    o = {
-      function()
-        telescope_builtin.oldfiles({ only_cwd = true })
-      end,
-      "Search through recently opened files",
-    },
-    -- This is still Lua as it might be lazily loaded
-    p = {
-      telescope.extensions.neoclip.default,
-      "Search through the clipboard to reassign the current paste",
-    },
+    o = { "<cmd>Telescope oldfiles only_cwd=true<cr>", "Search through recently opened files" },
+    p = { "<cmd>Telescope neoclip<cr>", "Search through the clipboard to reassign the current paste" },
     s = { "<cmd>Telescope search_history<cr>", "Lists recent search history" },
     t = { "<cmd>Telescope treesitter<cr>", "Search through treesitter tags" },
-    w = { require("telescope-tabs").list_tabs, "Search tabs" },
+    w = { "<cmd>Telescope telescope-tabs list_tabs<cr>", "Search tabs" },
   },
 
   g = {
--- nvim/lua/settings/plugins/telescope.lua
+++ nvim/lua/settings/plugins/telescope.lua
@@ -1,5 +1,6 @@
 local M = {
   "nvim-telescope/telescope.nvim",
+  cmd = "Telescope",
   version = false,
   dependencies = {
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
@@ -57,6 +58,7 @@ function M.config()
   telescope.load_extension("noice")
   telescope.load_extension("telescope-tabs")
   telescope.load_extension("luasnip")
+  telescope.load_extension("neoclip")
 
   local telescope_augroup_id = vim.api.nvim_create_augroup("telescope", { clear = true })
   vim.api.nvim_create_autocmd("FileType", {
@@ -65,33 +67,6 @@ function M.config()
     callback = function()
       vim.keymap.set("i", "<C-R>", "<C-R>", { silent = true, buffer = true })
     end,
-  })
-
-  local wk = require("which-key")
-
-  wk.register({
-    f = {
-      name = "+Telescope",
-      b = { "<cmd>Telescope buffers<cr>", "Find recent buffers" },
-      d = { "<cmd>Telescope dir find_files<cr>", "Find files in dir" },
-      f = { "<cmd>Telescope find_files<cr>", "Find files" },
-      g = { "<cmd>Telescope live_grep<cr>", "Grep through the project dir" },
-      h = { "<cmd>Telescope help_tags<cr>", "Search for help tags" },
-      j = { "<cmd>Telescope jumplist<cr>", "Search through jumplist" },
-      l = { "<cmd>Telescope current_buffer_fuzzy_find<cr>", "Search through lines in the buffer" },
-      o = { "<cmd>Telescope old_files only_cwd=true<cr>", "Search through recently opened files" },
-      -- This is still Lua as it might be lazily loaded
-      p = {
-        telescope.extensions.neoclip.default,
-        "Search through the clipboard to reassign the current paste",
-      },
-      s = { "<cmd>Telescope search_history<cr>", "Lists recent search history" },
-      t = { "<cmd>Telescope treesitter<cr>", "Search through treesitter tags" },
-      w = { require("telescope-tabs").list_tabs, "Search tabs" },
-    },
-  }, {
-    prefix = "<leader>",
-    noremap = true,
   })
 end
 
