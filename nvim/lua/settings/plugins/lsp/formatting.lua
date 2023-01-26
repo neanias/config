44e807de367774a15de66fca7860cccb33f66a3f
Will Mathewson <william.mathewson@freeagent.com>
2023-01-26 10:27:01 +0000

    Rework LSP formatting

--- nvim/lua/settings/plugins/lsp/formatting.lua
+++ nvim/lua/settings/plugins/lsp/formatting.lua
@@ -1,36 +1,30 @@
 local M = {}
 
 function M.format()
-  if vim.lsp.buf.format then
-    vim.lsp.buf.format()
-  else
-    vim.lsp.buf.formatting_sync()
-  end
+  local buf = vim.api.nvim_get_current_buf()
+  local ft = vim.bo[buf].filetype
+  local have_nls = #require("null-ls.sources").get_available(ft, "NULL_LS_FORMATTING") > 0
+
+  vim.lsp.buf.format({
+    timeout_ms = 2000,
+    bufnr = buf,
+    filter = function(client)
+      if have_nls then
+        return client.name == "null-ls"
+      else
+        return client.name ~= "null-ls"
+      end
+    end,
+  })
 end
 
 function M.setup(client, buf)
-  local ft = vim.api.nvim_buf_get_option(buf, "filetype")
-  local nls = require("settings.plugins.null-ls")
-
-  local enable = false
-  if nls.has_formatter(ft) then
-    enable = client.name == "null-ls"
-  else
-    enable = not (client.name == "null-ls")
-  end
-
-  if client.name == "tsserver" then
-    enable = false
-  end
-
-  client.server_capabilities.documentFormattingProvider = enable
   -- format on save
-  if client.server_capabilities.documentFormattingProvider then
-    local group = vim.api.nvim_create_augroup("LspFormat", { clear = true })
+  if client.supports_method("textDocument/formatting") then
     vim.api.nvim_create_autocmd("BufWritePre", {
-      buffer = vim.api.nvim_get_current_buf(),
-      callback = require("settings.plugins.lsp.formatting").format,
-      group = group,
+      buffer = buf,
+      callback = M.format,
+      group = vim.api.nvim_create_augroup("LspFormat." .. buf, {}),
     })
   end
 end
