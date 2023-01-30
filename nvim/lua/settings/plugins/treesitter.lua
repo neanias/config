f20f0f43d7368ed03f60356d0163181b01bbbbb1
Will Mathewson <william.mathewson@freeagent.com>
2023-01-30 10:44:13 +0000

    Expand query lint rules

--- nvim/lua/settings/plugins/treesitter.lua
+++ nvim/lua/settings/plugins/treesitter.lua
@@ -79,6 +79,8 @@ function M.config()
     },
     query_linter = {
       enable = true,
+      use_virtual_text = true,
+      lint_events = { "BufWrite", "CursorHold" },
     },
     refactor = {
       smart_rename = {
