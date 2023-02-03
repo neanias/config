ad0ececba0f64ebf7483dd17aa21e3cb1252d873
Will Mathewson <william.mathewson@freeagent.com>
2023-02-03 10:37:54 +0000

    Forget the current snippet when leaving insert mode

--- nvim/lua/settings/plugins/luasnip.lua
+++ nvim/lua/settings/plugins/luasnip.lua
@@ -17,6 +17,19 @@ function M.config()
     history = true,
     enable_autosnippets = true,
   })
+
+  -- Stop trying to jump through a snippet when changing modes
+  local unlinkgrp = vim.api.nvim_create_augroup("UnlinkSnippetOnModeChange", { clear = true })
+  vim.api.nvim_create_autocmd("ModeChanged", {
+    group = unlinkgrp,
+    pattern = { "s:n", "i:*" },
+    desc = "Forget the current snippet when leaving the insert mode",
+    callback = function(evt)
+      if luasnip.session and luasnip.session.current_nodes[evt.buf] and not luasnip.session.jump_active then
+        luasnip.unlink_current()
+      end
+    end,
+  })
 end
 
 return M
