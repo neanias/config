ab967649905803794d96112d9901e19732b19d31
Will Mathewson <william.mathewson@freeagent.com>
2023-02-03 10:34:42 +0000

    Load dial.nvim at startup

--- nvim/lua/settings/plugins/dial.lua
+++ nvim/lua/settings/plugins/dial.lua
@@ -1,9 +1,23 @@
 return {
   "monaqa/dial.nvim",
-  -- stylua: ignore
+  event = "VeryLazy",
   keys = {
-    { "<C-a>", function() return require("dial.map").inc_normal() end, expr = true, desc = "Increment" },
-    { "<C-x>", function() return require("dial.map").dec_normal() end, expr = true, desc = "Decrement" },
+    {
+      "<C-a>",
+      function()
+        return require("dial.map").inc_normal()
+      end,
+      expr = true,
+      desc = "Increment",
+    },
+    {
+      "<C-x>",
+      function()
+        return require("dial.map").dec_normal()
+      end,
+      expr = true,
+      desc = "Decrement",
+    },
   },
   config = function()
     local augend = require("dial.augend")
