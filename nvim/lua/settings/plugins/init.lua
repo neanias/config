e58a726d31f9f2d0578e5acf961687feb249402e
William Mathewson <wncmathewson@me.com>
2023-03-29 09:30:40 +0100

    Add nvim-spider plugin

--- nvim/lua/settings/plugins/init.lua
+++ nvim/lua/settings/plugins/init.lua
@@ -6,6 +6,43 @@ return {
   "williamboman/mason-lspconfig.nvim",
 
   {
+    "chrisgrieser/nvim-spider",
+    event = "VeryLazy",
+    -- lazy = false,
+    keys = {
+      {
+        "w",
+        function()
+          require("spider").motion("w")
+        end,
+        mode = { "n", "o", "x" },
+        desc = "Spider-w",
+      },
+      {
+        "e",
+        function()
+          require("spider").motion("e")
+        end,
+        mode = { "n", "o", "x" },
+        desc = "Spider-e",
+      },
+      {
+        "b",
+        function()
+          require("spider").motion("b")
+        end,
+        mode = { "n", "o", "x" },
+        desc = "Spider-b",
+      },
+      {
+        "ge",
+        function()
+          require("spider").motion("ge")
+        end,
+        mode = { "n", "o", "x" },
+        desc = "Spider-ge",
+      },
+    },
   },
 
   {
