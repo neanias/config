b1bc4a0a25cc4034b5dab39e785a623f13022c23
William Mathewson <wncmathewson@me.com>
2024-01-31 11:54:57 +0000

    Add tab_bar highlights into colour schemes
    It was quite convenient using the linking in `colours.lua`, but these
    should really be defined in the colour schemes themselves.

--- wezterm/colours.lua
+++ wezterm/colours.lua
@@ -31,24 +31,6 @@ end
 function colours.apply_color_scheme(config)
   config.color_scheme = scheme_for_appearance(get_appearance())
   config.color_scheme_dirs = { os.getenv("HOME") .. "/.config/wezterm/colours/" }
-
-  local scheme, _ = colours.fetch_colour_scheme()
-
-  config.colors = {
-    tab_bar = {
-      background = scheme.background,
-      new_tab = {
-        bg_color = scheme.background,
-        fg_color = scheme.ansi[8],
-        intensity = "Bold",
-      },
-      new_tab_hover = {
-        bg_color = scheme.ansi[1],
-        fg_color = scheme.brights[8],
-        intensity = "Bold",
-      },
-    },
-  }
 end
 
 return colours
