ceb98659831d6889f5b9d78c138eabe0536265db
Will Mathewson <will@cherub.health>
2025-07-02 12:45:48 +0100

    Add in LSP doc border

--- nvim/lua/settings/plugins/noice.lua
+++ nvim/lua/settings/plugins/noice.lua
@@ -17,7 +17,7 @@ return {
       bottom_search = true, -- use a classic bottom cmdline for search
       command_palette = true, -- position the cmdline and popupmenu together
       long_message_to_split = true, -- long messages will be sent to a split
-      lsp_doc_border = false, -- add a border to hover docs and signature help
+      lsp_doc_border = true, -- add a border to hover docs and signature help
     },
     routes = {
       -- Hide written message
